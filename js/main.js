69e716e11159c9cc412180e3c789e26cc6cbd2d2
giovannacta <gctacanada@gmail.com>
2023-12-01 21:22:18 -0800

    Added phone login

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.getElementById('sign-in-traditional')
 document.addEventListener('click', function () {
 
-    
+
     var emailTxt = document.getElementById('account_email').value;
     var passTxt = document.getElementById('account_passwd').value;
 
@@ -22,7 +22,7 @@ document.addEventListener('click', function () {
 window.addEventListener('load', function () {
     document.getElementById('gmail-login')
     document.addEventListener('click', function () {
-        
+
         var provider = new firebase.auth.GoogleAuthProvider();
         provider.addScope('email');
         firebase.auth().signInWithPopup(provider)
@@ -33,4 +33,34 @@ window.addEventListener('load', function () {
                 console.log('login fail', error);
             });
     });
+});
+
+function getPhoneNumberFromUserInput() {
+    return "+12368620940";
+}
+
+document.getElementById('phone-login')
+document.addEventListener('click', function () {
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+
+    const phoneNumber = getPhoneNumberFromUserInput();
+    const appVerifier = window.recaptchaVerifier;
+    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+        .then((confirmationResult) => {
+            // SMS sent. Prompt user to type the code from the message, then sign the
+            // user in with confirmationResult.confirm(code).
+            window.confirmationResult = confirmationResult;
+
+            const code = 123456;
+            confirmationResult.confirm(code).then((result) => {
+                // User signed in successfully.
+                const user = result.user;
+            }).catch((error) => {
+
+            });
+
+            // ...
+        }).catch((error) => {
+
+        });
 });
